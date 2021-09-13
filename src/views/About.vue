37a70e14b0a2dd5a55a66c219c0f4e480eb044e0
Matej Holubec <matej.holubec@gmail.com>
2021-09-13 18:16:41 +0200

    feat: ble basics

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <button @click="doBle">CLICK</button>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import BLABLA from "@/types/ble";
+
+@Component
+export default class About extends Vue {
+  private ble!: BLABLA;
+  private abl!: any;
+  a = [{}] as any[];
+
+  mounted() {
+    //@ts-ignore
+    this.ble = ble;
+    //@ts-ignore
+    this.abl = bluetoothle;
+    this.abl.initialize();
+  }
+  doBle() {
+ 
+    this.abl.startScan(
+      (a: any) => (a.name.includes("ESP") ? this.exit(a) : ""),
+      (a: any) => alert(JSON.stringify(a)),
+      {}
+    );
+
+    // this.ble.scan(
+    //   [],
+    //   7,
+    //   (data: any) => (this.a += JSON.stringify(data)),
+    //   (data: any) => (this.a += JSON.stringify(data))
+    // );
+  }
+  exit(a: any) {
+    this.abl.stopScan(() => null, () => true)
+    alert(atob(a.advertisement))
+  }
+}
+</script>
+
+<style scoped>
+</style>
